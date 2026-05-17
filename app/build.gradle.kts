41c8fed30c3f739ff18a7e23f631d45249aac66d
sinanalizz123-max <sinanalizz123-max@users.noreply.github.com>
2026-05-17 12:45:54 +0530

    build: Correct BlurView dependency version prefix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     implementation(libs.exifinterface)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
-    implementation("com.github.Dimezis:BlurView:2.0.3")
+    implementation("com.github.Dimezis:BlurView:version-2.0.3")
 }
 
 tasks.register("copyMappingFile") {
